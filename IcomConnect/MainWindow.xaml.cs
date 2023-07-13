8a18f90f6fcaba45603bf60fcb6a4ab3d5700d34
Björn Ekelund <bjorn@ekelund.nu>
2023-07-13 13:10:12 +0200

    Add project files.

---
+++ IcomConnect/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IcomConnect
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public const string RadioAddress = "192.168.1.12";
+        public const int RadioPortStart = 50001;
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            UdpClient udpClient = new UdpClient(RadioPortStart);
+            try
+            {
+                udpClient.Connect(RadioAddress, RadioPortStart);
+
+                // Sends a message to the host to which you have connected.
+                byte[] sendBytes = Encoding.ASCII.GetBytes("Is anybody there?");
+
+                udpClient.Send(sendBytes, sendBytes.Length);
+
+                // Sends a message to a different host using optional hostname and port parameters.
+                UdpClient udpClientB = new UdpClient();
+                udpClientB.Send(sendBytes, sendBytes.Length, "AlternateHostMachineName", 11000);
+
+                //IPEndPoint object will allow us to read datagrams sent from any source.
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                // Blocks until a message returns on this socket from a remote host.
+                byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                string returnData = Encoding.ASCII.GetString(receiveBytes);
+
+                // Uses the IPEndPoint object to determine which of these two hosts responded.
+                Console.WriteLine("This is the message you received " +
+                                             returnData.ToString());
+                Console.WriteLine("This message was sent from " +
+                                            RemoteIpEndPoint.Address.ToString() +
+                                            " on their port number " +
+                                            RemoteIpEndPoint.Port.ToString());
+
+                udpClient.Close();
+                udpClientB.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
